c7b82fef30773a28bb006e7ead6157d44185179b
edmarsoares <edmar1soares@gmail.com>
2019-10-30 20:44:38 -0300

    atualizando

---
+++ src/main/java/com/edmar/gerenciador_cursos_api/realtime/realT.ts
@@ -0,0 +1,33 @@
+// curso = {
+//     id:1,
+//     vagas: 20, 
+//     title: 'php'
+//   }
+  
+//   cursosLista: { id: 1, vagas: 20, 
+//      title: 'php'}[] = [];
+ 
+ 
+//    cursos: { [key:number]: Curso }[];
+ 
+//    cursosJson: {
+//      1: {
+//        id: 1,
+//        title: 'php',
+//        vagas: 20,
+//      },
+//      2: {
+ 
+//      }
+//    };
+ 
+//    onRealtimeEvent(event: {id: number; vagas: number}) {
+//      this.cursosLista.filter(curso => curso.id === id).forEach(curso => {
+//        curso.vagas = vagas;
+//      });
+     
+//      this.cursos.push(curso);
+//      this.cursos[id].vagas = vagas;
+ 
+//    }
+ 
